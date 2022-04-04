389c0cc8b5780e3853e4f86f72d25c10f39862c5
lhmoreno <lhmorenoti@hotmail.com>
2022-04-04 14:37:16 -0300

    style: Initial theme styled-components

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,19 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      primary: string
+      text: string
+      link: string
+      background: string
+      gray: {
+        dark: string
+        medium: string
+      }
+      hover: {
+        primary: string
+      }
+    }
+  }
+}
