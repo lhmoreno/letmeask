eda4725505966248232720dad06659ce92ea091c
lhmoreno <lhmorenoti@hotmail.com>
2022-04-26 17:04:44 -0300

    feat: Authentication firebase

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,9 +1,22 @@
 import { createContext, ReactNode, useEffect, useState } from 'react'
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+
+import accountImg from '../assets/images/account.png'
 
 type AuthStatus = 'authenticated' | 'unauthenticated' | 'pending'
 
+interface User {
+  id: string
+  name: string
+  avatar: string
+}
+
 interface UserContextProps {
   authStatus: AuthStatus
+  user?: User
+  signin: () => Promise<void>
+  signout: () => Promise<void>
 }
 
 interface UserProviderProps {
@@ -11,24 +24,53 @@ interface UserProviderProps {
 }
 
 export const UserContext = createContext<UserContextProps>({
-  authStatus: 'pending'
+  authStatus: 'pending',
+  signin: async () => {},
+  signout: async () => {}
 })
 
 export function UserProvider({ children }: UserProviderProps) {
+  const auth = getAuth()
+  const navigate = useNavigate()
   const [authStatus, setAuthStatus] = useState<AuthStatus>('pending')
+  const [user, setUser] = useState<User>()
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      setAuthStatus('unauthenticated')
-    }, 150)
+    const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+      if (authUser) {
+        const { displayName, photoURL, uid } = authUser
 
-    return () => clearTimeout(timeout)
+        setUser({
+          id: uid,
+          name: displayName || 'Batatão',
+          avatar: photoURL || accountImg
+        })
+        setAuthStatus('authenticated')
+      } else {
+        setAuthStatus('unauthenticated')
+      }
+    })
+
+    return () => unsubscribe()
   }, [])
+
+  async function signin() {
+    await signInWithPopup(auth, new GoogleAuthProvider)
+    navigate('/rooms')
+  }
+
+  async function signout() {
+    await signOut(auth)
+    navigate('/')
+  }
 
   return (
     <UserContext.Provider
       value={{
-        authStatus
+        authStatus,
+        user,
+        signin,
+        signout
       }}
     >
       { children }
