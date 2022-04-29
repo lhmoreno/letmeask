0c291adbc020d9f25c3d409ad1d19ee17f9bed0d
lhmoreno <lhmorenoti@hotmail.com>
2022-04-29 11:43:21 -0300

    feat: Button variants

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,15 +3,32 @@ import { ButtonHTMLAttributes } from 'react'
 import * as Styled from './styles'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
-  
+  variant?: 'primary' | 'gray'
 }
 
-function Button({ children, ...props }: ButtonProps) {
-  return (
-    <Styled.Container {...props}>
-      { children }
-    </Styled.Container>
-  )
+function Button({ variant, children, ...props }: ButtonProps) {
+  switch (variant) {
+    case 'primary':
+      return (
+        <Styled.ButtonPrimary {...props}>
+          { children }
+        </Styled.ButtonPrimary>
+      )
+
+    case 'gray':
+      return (
+        <Styled.ButtonGray {...props}>
+          { children }
+        </Styled.ButtonGray>
+      )
+
+    default:
+      return (
+        <Styled.ButtonPrimary {...props}>
+          { children }
+        </Styled.ButtonPrimary>
+      )
+  }
 }
 
 export default Button
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,24 +1,39 @@
-import styled from 'styled-components'
+import styled, { CSSObject } from 'styled-components'
 
-export const Container = styled.button(({ theme }) => {
+const defaultButtonStyles: CSSObject = {
+  height: '50px',
+  padding: '0px 32px',
+  cursor: 'pointer',
+  background: 'none',
+  border: 'none',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontWeight: '500',
+  transitionProperty: 'background',
+  transitionDuration: '0.3s'
+}
+
+export const ButtonPrimary = styled.button(({ theme }) => {
   return {
-    height: '50px',
-    padding: '0px 32px',
-    cursor: 'pointer',
-    background: 'none',
-    border: 'none',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: '8px',
-    fontWeight: '500',
-    color: theme.colors.white,
+    ...defaultButtonStyles,
     borderRadius: theme.rounded,
-    transitionProperty: 'background',
-    transitionDuration: '0.3s',
+    color: theme.colors.white,
     backgroundColor: theme.colors.primary,
     ":hover": {
       backgroundColor: theme.colors.primaryHover
+    }
+  }
+})
+
+export const ButtonGray = styled.button(({ theme }) => {
+  return {
+    ...defaultButtonStyles,
+    borderRadius: theme.rounded,
+    color: theme.colors.grayHover,
+    backgroundColor: '#DBDCDD',
+    ":hover": {
+      backgroundColor: '#CECECE'
     }
   }
 })
